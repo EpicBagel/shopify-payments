01d093d8d8a387f3d7bfacaa9074c4a532a76ba5
Jon Bolt <epic.bagel@gmail.com>
2013-10-23 15:06:29 +0100

    allowed URLS

--- shopify_payments/middleware/payment_barrier.py
+++ shopify_payments/middleware/payment_barrier.py
@@ -1,10 +1,11 @@
 from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.http import HttpResponseRedirect
 
 class PaymentBarrier(object):
 	def process_request(self, request):
 		upgrade_url = reverse('accounts:upgrade')
-		allowed_urls = ['shopify_payments:upgrade', 'shopify_payments:process-upgrade', 'shopify_payments:confirm-upgrade']
+		allowed_urls = ['shopify_payments:upgrade', 'shopify_payments:process-upgrade', 'shopify_payments:confirm-upgrade'] + getattr(settings, ALLOWED_UPGRADE_URLS, [])
 		# If no charge ID has been set, only allow the user on t
 		if request.user.is_authenticated() and not request.user.is_superuser:
 			if not request.company.charge_id and not request.path in [reverse(allowed_url) for allowed_url in allowed_urls]:
