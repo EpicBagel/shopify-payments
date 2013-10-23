1c990b2b769c38f8fc7f4472e128e8aab763efb7
Jon Bolt <epic.bagel@gmail.com>
2013-10-23 14:52:54 +0100

    Move to company

--- shopify_payments/middleware/payment_barrier.py
+++ shopify_payments/middleware/payment_barrier.py
@@ -6,5 +6,6 @@ class PaymentBarrier(object):
 		upgrade_url = reverse('accounts:upgrade')
 		allowed_urls = ['shopify_payments:upgrade', 'shopify_payments:process-upgrade', 'shopify_payments:confirm-upgrade']
 		# If no charge ID has been set, only allow the user on t
-		if request.user.is_authenticated() and not request.user.is_superuser and not request.user.charge_id and not request.path in [reverse(allowed_url) for allowed_url in allowed_urls]:
-			return HttpResponseRedirect(upgrade_url)
+		if request.user.is_authenticated() and not request.user.is_superuser:
+			if not request.company.charge_id and not request.path in [reverse(allowed_url) for allowed_url in allowed_urls]:
+				return HttpResponseRedirect(upgrade_url)
